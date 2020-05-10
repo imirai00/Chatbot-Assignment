9d16c9b0bcabd2ec79a7faa60e934e9f9a020160
imirai00 <62977426+imirai00@users.noreply.github.com>
2020-05-10 21:23:34 +0700

    update

---
+++ Server.js
@@ -0,0 +1,141 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var listUsers=[];
+
+io.on("connection", function(socket){
+  console.log("One user has connected " + socket.id);
+
+  // client send username to server
+  socket.on("Client-send-Username", function(data){
+    if (data != ""){
+      if(listUsers.indexOf(data)>=0){
+        socket.emit("Server-send-registration-fail");
+      }else{
+        listUsers.push(data);
+        socket.Username = data;
+        socket.emit("Server-send-registration-success", data);
+        io.sockets.emit("Server-send-users-list", listUsers);
+      }
+    } else {
+      socket.emit("Box-blank", "Box cannot be blank");
+    }
+  });
+
+  socket.on("Logout", function(){
+    listUsers.splice(
+      listUsers.indexOf(socket.Username), 1
+    );
+    socket.broadcast.emit("Server-send-users-list",listUsers);
+    listRoom.splice(0);
+    socket.broadcast.emit("Server-send-rooms-list", listRoom);
+  });
+
+  var listRoom=[];
+
+  socket.on("Check-room", function(data){
+    var DuplicateRoom = 0;
+    for(r in socket.adapter.rooms){
+      if (r == data){
+        DuplicateRoom = 1;
+      }
+    }
+    if (DuplicateRoom != 1){
+      socket.emit("Room-doesn't-exist", data);
+    } else {
+      socket.emit("Room-exist", data);
+    }
+
+  });
+    
+  let newRoom = {
+    name: String,
+    limit: Number
+  }
+
+  socket.on("Create-new-room", function(data){
+    if (data != ""){
+      socket.join(data);
+      socket.Room = data;
+      listRoom.splice(0);
+      var number;
+      let newRoom = {
+        'name': data,
+        'limit': number
+      }
+      socket.adapter.rooms[data].limit = 2;
+      listRoom.push(newRoom.name);
+      socket.emit("Server-send-room-success", newRoom.name);
+      io.sockets.emit("Server-send-rooms-list", listRoom);     
+    } else {
+      socket.emit("Box-blank", "Box cannot be blank");
+    }
+  });
+
+  socket.on("Join-room", function(data){
+    if (data != ""){
+      let filterRoom = listRoom.filter(function(room){
+        return room.name == data;
+      });
+
+      for(room in socket.adapter.rooms){
+        if(data == room){
+          if(socket.adapter.rooms[data].length < socket.adapter.rooms[data].limit){
+            listRoom.splice(0);
+            socket.join(room);
+            socket.Room=data;
+            socket.emit("Server-send-room-success", data);
+            io.sockets.emit("Server-send-rooms-list", listRoom);          
+          }else{
+            socket.emit("Reject-by-limited-members");
+            io.sockets.emit("Server-send-rooms-list", listRoom);  
+          }
+        }
+      }    
+    } else {
+      socket.emit("Box-blank", "Box cannot be blank");
+    }
+  });
+
+  socket.on("Leave", function(data){
+    socket.leave(data);
+    listRoom.splice(0);
+    io.sockets.emit("Server-send-rooms-list", listRoom);
+  });
+
+  socket.on("User-send-message", function(data){
+    io.sockets.in(socket.Room).emit("Server-send-mesage", formatMessage(socket.Username, data) );
+  });
+
+  socket.on("User-is-typing", function(){
+    var s = socket.Username + " is typing";
+    io.sockets.emit("Someone-is-typing", s);
+  });
+
+  socket.on("User-stop-typing", function(){
+    io.sockets.emit("Someone-stop-typing");
+  });
+});
+
+app.get("/", function(req, res){
+  res.render("Home");
+});
+
+var moment = require('moment');
+
+function formatMessage(username, text){
+return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+    }
+}
+
+
---
+++ public/Client.js
@@ -0,0 +1,157 @@
+var socket = io("http://localhost:3000");
+
+socket.on("Server-send-registration-fail", function(){
+  alert("The Username has been registered");
+});
+
+socket.on("Box-blank", function(data){
+  alert(data);
+});
+
+socket.on("Server-send-users-list", function(data){
+  $("#boxContentuser1").html("");
+  $("#boxContentuser2").html("");
+  data.forEach(function(i){
+    $("#boxContentuser1").append("<div class='user'>" + i + "</div>");
+    $("#boxContentuser2").append("<div class='user'>" + i + "</div>");
+  });
+});
+
+socket.on("Server-send-registration-success", function(data){
+  $("#currentUser1").html(data);
+  $("#currentUser2").html(data);
+  $("#loginForm").hide(0);
+  $("#roomForm").show(0);
+  $("#chatForm").hide(0);
+});
+
+socket.on("Room-doesn't-exist", function(data){  
+    socket.emit("Create-new-room", data);
+});
+
+socket.on("Room-exist", function(data){  
+    socket.emit("Join-room", data);
+});
+
+socket.on("Reject-by-limited-members", function(){
+    alert("This room is full");
+})
+
+socket.on("Server-send-rooms-list", function(data){
+  $("#boxContentroom").html("");
+  data.forEach(function(r){
+    $("#boxContentroom").append("<div class='room'>" + r + "</div>");
+  });
+});
+
+socket.on("Server-send-room-success", function(data){
+  $("#currentRoom").html(data);
+  $("#loginForm").hide(0);
+  $("#roomForm").hide(0);
+  $("#chatForm").show(0);
+});
+
+socket.on("Server-send-mesage", function(data){
+  console.log(data);
+  outputMessage(data);
+});
+
+function outputMessage(data) {
+    const div = document.createElement('div');
+    div.classList.add('Server-send-mesage');
+    div.innerHTML = `<div class="message">
+    <p class="meta">${data.username} <span>${data.time}</span></p>
+    <p class="text">${data.text}</p>`;
+    document.querySelector('#listMessages').appendChild(div);
+}
+
+socket.on("Someone-is-typing", function(data){
+  $("#notification").html("<img width='20px' src='typing05.gif'> " + data);
+});
+
+socket.on("Someone-stop-typing", function(){
+  $("#notification").html("");
+});
+
+$(document).ready(function(){
+  $("#loginForm").show(0);
+  $("#roomForm").hide(0);
+  $("#chatForm").hide(0);
+
+  $("#btnRegister").click(function(){
+    socket.emit("Client-send-Username", $("#txtUsername").val());
+    $("#txtUsername").val("");
+  });
+
+  $(".Register-on-enter").keydown(function(event){
+    // Enter key has keyCode = 13
+    if (event.keyCode == 13) {
+      var register = $("#txtUsername").val();
+      socket.emit("Client-send-Username", register);
+      this.reset;
+      $("#txtUsername").val("");
+      return false;
+    }
+  })
+
+  $("#btnLogout1").click(function(){
+    socket.emit("Logout");
+    $("#chatForm").hide(0);
+    $("#roomForm").hide(0);
+    $("#loginForm").show(0);
+  });
+
+  $("#btnLogout2").click(function(){
+    socket.emit("Logout");
+    $("#chatForm").hide(0);
+    $("#roomForm").hide(0);
+    $("#loginForm").show(0);
+  });
+
+  $("#btnSendRoom").click(function(){
+    socket.emit("Check-room", $("#txtRoom").val());
+    $("#txtRoom").val("");
+  });
+
+  $(".Submit-room-on-enter").keydown(function(event){
+    // Enter key has keyCode = 13
+    if (event.keyCode == 13) {
+      var room = $("#txtRoom").val();
+      socket.emit("Check-room", room);
+      this.reset;
+      $("#txtRoom").val("");
+      return false;
+    }
+  })
+
+  $("#btnLeave").click(function(){
+    socket.emit("Leave", $("#currentRoom").val());
+    $("#chatForm").hide(0);
+    $("#roomForm").show(0);
+    $("#loginForm").hide(0);
+  });
+
+  $("#btnSendMessage").click(function(){
+    socket.emit("User-send-message", $("#txtMessage").val());
+    $("#txtMessage").val("");
+  });
+
+  $(".Submit-on-enter").keydown(function(event){
+    // Enter key has keyCode = 13
+    if (event.keyCode == 13) {
+      var message = $("#txtMessage").val();
+      socket.emit("User-send-message", message);
+      this.reset;
+      $("#txtMessage").val("");
+      return false;
+    }
+  })
+
+  $("#txtMessage").focusin(function(){
+    socket.emit("User-is-typing");
+  })
+
+  $("#txtMessage").focusout(function(){
+    socket.emit("User-stop-typing");
+  })
+});
